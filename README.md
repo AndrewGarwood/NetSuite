4c7d63319f945801c14640ec9b79a22665e425be
Andrew Garwood <andrew.garwood.949@gmail.com>
2025-06-12 09:49:58 -0700

    updated samplePayloads.ts to new type definition. switched to reference links in README.md.

--- README.md
+++ README.md
@@ -1,20 +1,39 @@
 # Overview:
-- A collection of scripts using [NetSuite's](https://www.netsuite.com/portal/home.shtml) [SuiteScript 2.0](https://docs.oracle.com/en/cloud/saas/netsuite/ns-online-help/article_4140956840.html) to implement NetSuite as company's ERP.
-- There are multiple entry points to create/update/delete data from one's NetSuite account/instance. I prefer the REST entry point: [NetSuite Applications Suite - SuiteScript 2.x RESTlet Script Type](https://docs.oracle.com/en/cloud/saas/netsuite/ns-online-help/section_4387799403.html)
-- An essential reference: NetSuite's [Record Browser](https://system.netsuite.com/help/helpcenter/en_US/srbrowser/Browser2024_2/script/record/account.html) 
+- A collection of scripts using [NetSuite's][netsuite_home] [SuiteScript 2.0][suitescript_docs] to implement NetSuite as company's ERP.
+- There are multiple entry points to create/update/delete data from one's NetSuite account/instance. I prefer the REST entry point: [NetSuite Applications Suite - SuiteScript 2.x RESTlet Script Type][restlet_docs]
+- An essential reference: NetSuite's [Record Browser][record_browser] to determine the structure & properties of records and subrecords.
 
-**Note: In process of rewriting/refactoring some files after simplifying the data structures used in Post Request Bodies.** (e.g. in [PostRequests.ts](https://github.com/AndrewGarwood/NetSuite/blob/master/SuiteCloud/src/utils/api/types/PostRequests.ts) and [Api.ts](https://github.com/AndrewGarwood/NetSuite/blob/master/SuiteCloud/src/utils/api/types/Api.ts))
+**Note: In process of rewriting/refactoring some files after simplifying the data structures used in Post Request Bodies.** 
+- (i.e. I simplified the types in [PostRequests.ts][post_requests_file] and [Api.ts][api_file] because I decided to make [POST_UpsertRecord.js][upsert_file] handle the complexity of conforming to SuiteScript's syntax/data structures; So creating payloads will be easier. (e.g. compare [samplePayloads.ts][sample_payloads_file]'s [previous format][old_post_options_image] to its [new format][new_post_options_image]))
 
 ### My current approach is as follows:
 0. Determine desired objective. For example, if I need to upload a substantial amount of data, I can create records through POST requests 
-1. Write API endpoints so I can make requests to them with paylaods (e.g. [POST_UpsertRecord.js](https://github.com/AndrewGarwood/NetSuite/blob/master/SuiteCloud/src/FileCabinet/SuiteScripts/REST/POST/POST_UpsertRecord.js))
-2. I wanted to use TypeScript in VSCode, so I set up an Oauth2.0 flow to communicate with these endpoints (see [SuiteCloud/src/server/authServer.ts](https://github.com/AndrewGarwood/NetSuite/blob/master/SuiteCloud/src/server/authServer.ts))
-3. In my use case, the goal is to read data from csv files and store them into payloads
-4. Determine proper mapping by using aforementioned [Record Browser](https://system.netsuite.com/help/helpcenter/en_US/srbrowser/Browser2024_2/script/record/account.html) ~~and store it into ParseOption objects.~~
-5. (deprecated, pending rewrite) ~~Then use ParseOptions[] as a parameter of [parseCsvToPostRecordOptions](https://github.com/AndrewGarwood/NetSuite/blob/master/SuiteCloud/src/parseCsvToRequestBody.ts)~~
-6. (pending tests after rewrite) Use authorization tokens generated from authorization flow to make API calls, e.g. in [SuiteCloud/src/parses/parseEntity.ts](https://github.com/AndrewGarwood/NetSuite/blob/master/SuiteCloud/src/parses/parseEntity.ts) using functions from [SuiteCloud/src/utils/api/callApi.ts](https://github.com/AndrewGarwood/NetSuite/blob/master/SuiteCloud/src/utils/api/callApi.ts)
+1. Write API endpoints so I can make requests to them with paylaods (e.g. [POST_UpsertRecord.js][upsert_file] for creating/updating records)
+2. I wanted to use TypeScript in VSCode, so I set up an Oauth2.0 flow to communicate with these endpoints (see [authServer.ts][oauth_file])
+3. In my use case, the goal is to read data from csv files and store them into [payloads][sample_payloads_file]
+4. Determine proper mapping by using aforementioned [Record Browser][record_browser] ~~and store it into ParseOption objects.~~
+5. (deprecated, pending rewrite) ~~Then use ParseOptions[] as a parameter of parseCsvToPostRecordOptions~~
+6. (pending tests after rewrite) Use authorization tokens generated from authorization flow to make API calls, e.g. in [parseEntity.ts][parse_entity_file] using functions from [callApi.ts][call_api_file]
 7. Write more features/improvements and refactor as work continues.
 
 #### Guides I found helpful for using SuiteScript: 
-- [NetSuite RESTlet Creation - SuiteScript 2.0 with OAuth 2 - YouTube](https://www.youtube.com/watch?v=MAOMQp5dh0U)
-- [The SuiteScript Developer's Guide on NetSuite Subrecords - NetSuite Insights](https://netsuite.smash-ict.com/suitescript-developers-guide-on-netsuite-subrecords-part-1/)
+- [NetSuite RESTlet Creation - SuiteScript 2.0 with OAuth 2 - YouTube][oauth_video]
+- [The SuiteScript Developer's Guide on NetSuite Subrecords - NetSuite Insights][subrecord_guide]
+
+## Links
+-----
+[netsuite_home]: https://www.netsuite.com/portal/home.shtml
+[suitescript_docs]: https://docs.oracle.com/en/cloud/saas/netsuite/ns-online-help/article_4140956840.html
+[restlet_docs]: https://docs.oracle.com/en/cloud/saas/netsuite/ns-online-help/section_4387799403.html
+[record_browser]: https://system.netsuite.com/help/helpcenter/en_US/srbrowser/Browser2024_2/script/record/account.html
+[post_requests_file]: https://github.com/AndrewGarwood/NetSuite/blob/master/SuiteCloud/src/utils/api/types/PostRequests.ts
+[api_file]: https://github.com/AndrewGarwood/NetSuite/blob/master/SuiteCloud/src/utils/api/types/Api.ts
+[upsert_file]: https://github.com/AndrewGarwood/NetSuite/blob/master/SuiteCloud/src/FileCabinet/SuiteScripts/REST/POST/UpsertRecord.js
+[ouath_file]: https://github.com/AndrewGarwood/NetSuite/blob/master/SuiteCloud/src/server/authServer.ts
+[sample_payloads_file]: https://github.com/AndrewGarwood/NetSuite/blob/master/SuiteCloud/src/utils/api/samplePayloads.ts
+[parse_entity_file]: https://github.com/AndrewGarwood/NetSuite/blob/master/SuiteCloud/src/parses/parseEntity.ts
+[call_api_file]: https://github.com/AndrewGarwood/NetSuite/blob/master/SuiteCloud/src/utils/api/callApi.ts
+[oauth_video]: https://www.youtube.com/watch?v=MAOMQp5dh0U
+[subrecord_guide]: https://netsuite.smash-ict.com/suitescript-developers-guide-on-netsuite-subrecords-part-1/
+[old_post_options_image]: ./images/Old%20PostRecordOptions.png
+[new_post_options_image]: ./images/New%20PostRecordOptions.png
--- SuiteCloud/src/utils/api/samplePayloads.ts
+++ SuiteCloud/src/utils/api/samplePayloads.ts
@@ -1,152 +1,104 @@
 /**
- * @deprecated . need to replace with new sample payloads using new type versions. {@link FieldDictionary} and {@link SublistDictionary}
  * @file src/utils/api/samplePayloads.ts
  * @description Sample payloads for testing API calls
  */
-import { 
-    SublistDictionary, PostRecordOptions, FieldDictionary, 
-    // SetSubrecordOptions, SublistFieldDictionary, 
-    SetSublistValueOptions, 
-    SetFieldValueOptions
-} from "src/utils/api/types"
+
+import { CountryAbbreviationEnum, StateAbbreviationEnum, RecordTypeEnum } from "../ns";
 import { RADIO_FIELD_FALSE } from "../typeValidation";
-import { NetSuiteCountryEnum, CountryAbbreviationEnum as COUNTRIES, StateAbbreviationEnum as STATES } from "src/utils/ns"
-import { RecordTypeEnum } from "src/utils/ns/record/Record"
+import { SetSublistSubrecordOptions, FieldDictionary, SetFieldValueOptions, 
+    PostRecordOptions, SetSublistValueOptions, SublistDictionary, SublistLine } from "./types";
 
-const NOT_INACTIVE = false;
+const NOT_INACTIVE = false; // const IS_INACTIVE = true;
 const NOT_DYNAMIC = false;
-// const IS_INACTIVE = true;
 
-/**@deprecated */
-const ODEGAARD_ADDRESS_SUBRECORD_OPTIONS: SetSubrecordOptions = {
-    parentSublistId: 'addressbook',
-    line: 0,
+const ODEGAARD_ADDRESS_SUBRECORD_OPTIONS: SetSublistSubrecordOptions = {
+    sublistId: 'addressbook',
     fieldId: 'addressbookaddress',
     subrecordType: 'address',
-    fieldDict: {
-        valueFields: [
-            { fieldId: 'country', value: COUNTRIES.UNITED_STATES },
-            { fieldId: 'addr1', value: '4060 George Washington Ln.' },
-            { fieldId: 'addr2', value: 'Room 326' },
-            { fieldId: 'addressee', value: 'dubs' },
-            { fieldId: 'addrphone', value: '206-543-2990' },
-            { fieldId: 'city', value: 'Seattle' },
-            { fieldId: 'state', value: STATES.WASHINGTON },
-            { fieldId: 'zip', value: '98105' },
-        ]
+    fields: {
+        'country': CountryAbbreviationEnum.UNITED_STATES,
+        'addr1': '4060 George Washington Ln.',
+        'addr2': 'Room 326',
+        'addressee': 'dubs',
+        'addrphone': '206-543-2990',
+        'city': 'Seattle',
+        'state': StateAbbreviationEnum.WASHINGTON,
+        'zip': '98105',
     } as FieldDictionary,
-} as SetSubrecordOptions
+};
 
-/**@deprecated */
-const SUZZALLO_ADDRESS_SUBRECORD_OPTIONS: SetSubrecordOptions = {
-    parentSublistId: 'addressbook',
-    line: 1,
+const SUZZALLO_ADDRESS_SUBRECORD_OPTIONS: SetSublistSubrecordOptions = {
+    sublistId: 'addressbook',
     fieldId: 'addressbookaddress',
     subrecordType: 'address',
-    fieldDict: {
-        valueFields: [
-            { fieldId: 'country', value: COUNTRIES.UNITED_STATES},
-            { fieldId: 'addr1', value: '4000 15th Ave NE' },
-            { fieldId: 'addr2', value: 'Room 102' },
-            { fieldId: 'addressee', value: 'dubs' },
-            { fieldId: 'addrphone', value: '206-543-0242' },
-            { fieldId: 'city', value: 'Seattle' },
-            { fieldId: 'state', value: STATES.WASHINGTON },
-            { fieldId: 'zip', value: '98195' },
-        ] as SetFieldValueOptions[],
+    fields: {
+        'country': CountryAbbreviationEnum.UNITED_STATES,
+        'addr1': '4000 15th Ave NE',
+        'addr2': 'Room 102',
+        'addressee': 'dubs',
+        'addrphone': '206-543-0242',
+        'city': 'Seattle',
+        'state': StateAbbreviationEnum.WASHINGTON,
+        'zip': '98195',
     } as FieldDictionary,
-}
+};
 
-/**@deprecated */
-export const UW_LIBRARIES_UPSERT_VENDOR_OPTIONS: PostRecordOptions = {
+export const UW_LIBRARIES_POST_VENDOR_OPTIONS: PostRecordOptions = {
     recordType: RecordTypeEnum.VENDOR,
-    fieldDict: {
-        valueFields: [
-            { fieldId: 'internalid', value: 5711 },
-            { fieldId: 'phone', value: '206-543-2990' },
-            { fieldId: 'email', value: 'awg1024@uw.edu' }
-        ] as SetFieldValueOptions[],
+    isDynamic: NOT_DYNAMIC,
+    fields: {
+        'entityid': 'UW_LIBRARIES',
+        'externalid': 'UW_LIBRARIES',
+        'isperson': RADIO_FIELD_FALSE,  
+        'companyname': 'UW Libraries',
     } as FieldDictionary,
-}
-
-/**@deprecated */
-export const UW_LIBRARIES_CREATE_VENDOR_OPTIONS: PostRecordOptions = {
-    recordType: RecordTypeEnum.VENDOR,
-    // isDynamic: NOT_DYNAMIC,
-    fieldDict: {
-        valueFields: [
-            { fieldId: 'entityid', value: 'UW_LIBRARIES' },
-            { fieldId: 'externalid', value: 'UW_LIBRARIES' },
-            { fieldId: 'isperson', value: RADIO_FIELD_FALSE },  
-            { fieldId: 'companyname', value: 'UW Libraries' },
-            // { fieldId: 'isinactive', value: NOT_INACTIVE },
-        ],
-    } as FieldDictionary,
-    sublistDict: {
-        'addressbook': {
-            valueFields: [
-                { 
-                    sublistId: 'addressbook', 
-                    fieldId: 'label', 
-                    line: 0, 
-                    value: 'Odegaard Library' 
-                } as SetSublistValueOptions,                
-                { 
-                    sublistId: 'addressbook', 
-                    fieldId: 'label', 
-                    line: 1, 
-                    value: 'Suzzallo and Allen Libraries' 
-                } as SetSublistValueOptions,
-            ] as SetSublistValueOptions[],
-            subrecordFields: [
-                ODEGAARD_ADDRESS_SUBRECORD_OPTIONS, 
-                SUZZALLO_ADDRESS_SUBRECORD_OPTIONS
-            ] as SetSubrecordOptions[],
-        } as SublistFieldDictionary
+    sublists: {
+        addressbook: [
+            {
+                label: 'Odegaard Library' , 
+                line: 0,
+                addressbookaddress: ODEGAARD_ADDRESS_SUBRECORD_OPTIONS,
+            },                
+            { 
+                label: 'Suzzallo and Allen Libraries', 
+                line: 1, 
+                addressbookaddress: SUZZALLO_ADDRESS_SUBRECORD_OPTIONS, 
+            } 
+        ] as SublistLine[]
     } as SublistDictionary,
-}
+};
 
-/**@deprecated */
+const MISSION_VIEJO_LIBRARY_ADDRESS_SUBRECORD_OPTIONS: SetSublistSubrecordOptions = {
+    sublistId: 'addressbook',
+    fieldId: 'addressbookaddress',
+    subrecordType: 'address',
+    fields: {
+        'country': CountryAbbreviationEnum.UNITED_STATES,
+        'addr1': '100 Civic Center',
+        'addressee': 'Library Addressee',
+        'attention': 'Library Attention',
+        'addrphone': '949-830-7100',
+        'city': 'Mission Viejo',
+        'state': StateAbbreviationEnum.CALIFORNIA,
+        'zip': '92691',
+    } as FieldDictionary,
+};
+
 export const MISSION_VIEJO_LIBRARY_CREATE_VENDOR_OPTIONS: PostRecordOptions = {
     recordType: RecordTypeEnum.VENDOR,
-    // isDynamic: NOT_DYNAMIC,
-    fieldDict: {
-        valueFields: [
-            { fieldId: 'companyname', value: 'City of Mission Viejo' },
-            { fieldId: 'isperson', value: RADIO_FIELD_FALSE },  
-            { fieldId: 'isinactive', value: NOT_INACTIVE },
-        ] as SetFieldValueOptions[],
+    isDynamic: NOT_DYNAMIC,
+    fields: {
+        'companyname': 'City of Mission Viejo',
+        'isperson': RADIO_FIELD_FALSE,  
+        'isinactive': NOT_INACTIVE,
     } as FieldDictionary,
-    sublistDict: {
-        'addressbook': {
-            valueFields: [
-                { 
-                    sublistId: 'addressbook', 
-                    fieldId: 'label', 
-                    line: 0, 
-                    value: 'Primary Address' 
-                } as SetSublistValueOptions,    
-            ],
-            subrecordFields: [
-                {
-                    parentSublistId: 'addressbook',
-                    line: 0,
-                    fieldId: 'addressbookaddress',
-                    subrecordType: 'address',
-                    fieldDict: {
-                        valueFields: [
-                            { fieldId: 'country', value: COUNTRIES.UNITED_STATES},
-                            { fieldId: 'addr1', value: '100 Civic Center' },
-                            { fieldId: 'addressee', value: 'Library Addressee' },
-                            { fieldId: 'attention', value: 'Library Attention' },
-                            { fieldId: 'addrphone', value: '949-830-7100' },
-                            { fieldId: 'city', value: 'Mission Viejo' },
-                            { fieldId: 'state', value: STATES.CALIFORNIA },
-                            { fieldId: 'zip', value: '92691' },
-                        ]
-                    } as FieldDictionary,
-                } as SetSubrecordOptions
-            ] as SetSubrecordOptions[],
-        } as SublistFieldDictionary
+    sublists: {
+        'addressbook': [
+            {  
+                line: 0, 
+                label: 'Primary Address',
+                addressbookaddress: MISSION_VIEJO_LIBRARY_ADDRESS_SUBRECORD_OPTIONS,     
+            }
+        ] as SublistLine[],
     } as SublistDictionary,
-}
+};
